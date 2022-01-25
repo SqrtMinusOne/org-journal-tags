1599f526a921540dfc0bdc0d725a7019ec2738d3
SqrtMinusOne <thexcloud@gmail.com>
2022-01-25 16:25:04 +0300

    initial commit

---
+++ org-journal-tags.el
@@ -0,0 +1,34 @@
+;;; org-journal-tags.el --- TODO -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Korytov Pavel
+
+;; Author: Korytov Pavel <thexcloud@gmail.com>
+;; Maintainer: Korytov Pavel <thexcloud@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1") (org-journal "2.1.2"))
+;; Homepage: https://github.com/SqrtMinusOne/org-journal-tags.el
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
+(require 'org-journal)
+
+(provide 'org-journal-tags)
+;;; org-journal-tags.el ends here
